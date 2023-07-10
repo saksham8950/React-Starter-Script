656241d8f1d5e9b275ac56dc180cda0f48f8c675
Saksham <sakshamchopra174@gmail.com>
2023-07-10 18:43:38 +0530

    React-Starter-Script

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import "./app.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a>Learn React From Me</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
